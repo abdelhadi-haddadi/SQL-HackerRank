7902d7ec5feb4482fa03ce8fc96ccc5005b4724d
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2025-02-04 12:56:32 +0100

    Update easy.sql

--- easy.sql
+++ easy.sql
@@ -4,3 +4,7 @@ select * from CITY ;
 -- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA --
 select * from CITY where POPULATION > 100000 and CountryCode = "USA";
 
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+select NAME from CITY where POPULATION > 120000 and CountryCode = "USA"
+  
+
